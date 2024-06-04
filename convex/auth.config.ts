47eabce74fb9470d3759e354cf150ccc1906ca2b
Fred P. Majoni <fred@trafficsafety.co.zw>
2024-06-04 17:19:25 +0200

    new

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://together-mosquito-56.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['media.istockphoto.com', 't3.ftcdn.net', 'striped-stork-296.convex.cloud', 'upload.wikimedia.org', 'images.unsplash.com'],
+    },
+};
+
+export default nextConfig;
